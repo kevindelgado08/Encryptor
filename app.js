54e372b3f2589841a1b736277cb0d65f3520cf77
Kevin Delgado Rojas <delgadorojas.kevin@hotmail.com>
2024-03-11 16:58:52 -0600

    Comiezo de pruebas de lógica de javascript

--- app.js
+++ app.js
@@ -1,6 +1,36 @@
 /*
 This code is developed by Kevin Delgado Rojas @ Alura Latam
 */
+const ingresoTexto = document.getElementById("ingresoTexto");
+
+const botonEncriptar = document.getElementById("botonEncriptar");
+
+const botonDesencriptar = document.getElementById("botonDesencriptar");
+
+const botonCopiar = document.getElementById("botonDesencriptar");
+
+const mensaje1 = document.getElementById("mensaje1");
+
+/* 
+e = enter
+i = imes
+a = ai
+o = ober
+u = ufat
+*/
+let reemplazo = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+]
+
+botonEncriptar.addEventListener("click", () => {
+    const texto1=ingresoTexto.value.toLowerCase()
+    console.log(texto1)
+})
+
 
 function encriptar(params) {
     
